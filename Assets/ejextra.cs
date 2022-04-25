d8fb172b2f2eb5bf9495aacc8048d7b5e76b3048
nicolas-chab <47203227@est.ort.edu.ar>
2022-04-25 15:27:41 -0300

    recuperatorio hecho(practica)

---
+++ Assets/ejextra.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ejextra : MonoBehaviour
+{
+    public int años;
+    public float altura;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(años < 1)
+        {
+            Debug.Log("error la edad debe ser mayor a 1");
+        }
+        else
+    {
+
+        
+        if(años > 13 && altura > 1.2)
+      {
+            Debug.Log("acceso permitido");
+      }
+        
+        else
+      {
+            Debug.Log("acceso denegado");
+        }
+    }
+        if (altura < 0)
+      {
+            Debug.Log("Error, la altura debe ser positiva");
+      }
+        else
+        {
+
+
+            if (años > 13 && altura > 1.2)
+            {
+                Debug.Log("acceso permitido");
+            }
+
+            else
+            {
+                Debug.Log("acceso denegado");
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/recuperatorio.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class recuperatorio : MonoBehaviour
+{
+    public string gusto;
+    public int cantidad;
+    float precio = 500;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (cantidad >= 250 && cantidad <= 3000)
+        {
+
+            if (gusto == "CHO" && cantidad == 250)
+            {
+                precio = 500 / 4;
+                Debug.Log("El precio va a ser $" + precio);
+            }
+            if (gusto == "CHO" && cantidad == 500)
+            {
+                precio = 500 / 2;
+                Debug.Log("El precio va a ser $" + precio);
+            }
+            if (gusto == "CHO" && cantidad == 1000)
+            {
+                precio = 500;
+                Debug.Log("El precio va a ser $" + precio);
+            }
+            if (gusto == "DDL" && cantidad == 250)
+            {
+                precio = 500 / 4;
+                Debug.Log("El precio va a ser $" + precio);
+            }
+            if (gusto == "DDL" && cantidad == 500)
+            {
+                precio = 500 / 2;
+                Debug.Log("El precio va a ser $" + precio);
+            }
+            if (gusto == "DDL" && cantidad == 1000)
+            {
+                precio = 500;
+                Debug.Log("El precio va a ser $" + precio);
+            }
+            if (gusto == "FRU" && cantidad == 250)
+            {
+                precio = (500 / 4) * 0.9f;
+                Debug.Log("El precio va a ser $" + precio);
+            }
+            if (gusto == "FRU" && cantidad == 500)
+            {
+                precio = (500 / 2) * 0.9f;
+                Debug.Log("El precio va a ser $" + precio);
+            }
+            if (gusto == "FRU" && cantidad == 1000)
+            {
+                precio = 500 * 0.9f;
+                Debug.Log("El precio va a ser $" + precio);
+            }
+        }
+        if (cantidad < 250 && cantidad > 3000)
+        {
+            Debug.Log("La cantidad debe ser mayor a 250 y menor a 3000");
+        }
+        if (gusto != "CHO" && gusto != "DDL" && gusto != "FRU")
+        {
+            Debug.Log("Poner un gusto valido");
+        }
+
+        // Update is called once per frame
+        void Update()
+     {
+        
+     }
+   }
+}
